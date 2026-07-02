68116532d279553c56b2768ff2a256868699dbd9
Heritier Moise <vitalheritier1@gmail.com>
2026-07-02 20:50:24 +0300

    mes modifications pour acces a la base

--- config/database.php
+++ config/database.php
@@ -32,8 +32,8 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('DB_MYSQL_ATTR_SSL_CA')
-                    ? base_path(env('DB_MYSQL_ATTR_SSL_CA'))
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA', env('DB_MYSQL_ATTR_SSL_CA'))
+                    ? base_path(env('MYSQL_ATTR_SSL_CA', env('DB_MYSQL_ATTR_SSL_CA')))
                     : base_path('cert/ca.pem'),
                 PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
             ]) : [],
@@ -55,8 +55,8 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('DB_MYSQL_ATTR_SSL_CA')
-                    ? base_path(env('DB_MYSQL_ATTR_SSL_CA'))
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA', env('DB_MYSQL_ATTR_SSL_CA'))
+                    ? base_path(env('MYSQL_ATTR_SSL_CA', env('DB_MYSQL_ATTR_SSL_CA')))
                     : base_path('cert/ca.pem'),
                 PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
             ]) : [],
