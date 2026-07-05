da3132bf11be77d9d52400417b238053dc41aba2
Heritier Moise <vitalheritier1@gmail.com>
2026-07-05 21:53:17 +0300

    modification de ssl

--- config/database.php
+++ config/database.php
@@ -32,11 +32,8 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env(
-                    'DB_MYSQL_ATTR_SSL_CA',
-                    env('MYSQL_ATTR_SSL_CA', base_path('cert/ca.pem'))
-                ),
-                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => true,
+                PDO::MYSQL_ATTR_SSL_CA => env('DB_MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => env('DB_MYSQL_ATTR_SSL_VERIFY_SERVER_CERT', false),
             ]) : [],
         ],
 
@@ -56,11 +53,8 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env(
-                    'DB_MYSQL_ATTR_SSL_CA',
-                    env('MYSQL_ATTR_SSL_CA', base_path('cert/ca.pem'))
-                ),
-                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => true,
+                PDO::MYSQL_ATTR_SSL_CA => env('DB_MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => env('DB_MYSQL_ATTR_SSL_VERIFY_SERVER_CERT', false),
             ]) : [],
         ],
 
